938b80c55c27642f9d12fe903f1463db49d29bd9
Pablo Duboue <pablo.duboue@gmail.com>
2017-01-08 16:39:54 -0500

    Roll subsystem (Closes #1)

--- app/handlers/dice.py
+++ app/handlers/dice.py
@@ -1,29 +1,121 @@
 import logging
-from config.settings import relay, server_name_config
+from config.settings import owner_email, silent, server_name_config, relay
 from salmon.routing import route, route_like, stateless, Router
 from salmon import view
 from app.model.unicode_helper import safe_unicode
+from salmon.bounce import bounce_to
+from salmon.mail import MailResponse
+from salmon.server import SMTPError
+from app.model.emails import tst_email_processed, send_or_queue, sanitize
+import app.model.campaign as c
+from app.model.campaign import INTERNAL, UNKNOWN
+from app.model.dice import find_roll, execute_roll, add_roll, RollStrParseException
+
 
 @route("pm-dice-(rollid)@(host)", rollid="\d+")
 @stateless
 def DICE(message, rollid, host=None):
+    service_address = 'pm-dice-' + rollid
+    server_name = server_name_config
+
+    # check the sender is known
     sender = place_sender(message)
     if sender == INTERNAL:
+        logging.debug(u"INTERNAL ignoring %s@%s from %s" %
+                      (service_address, server_name, message['from']))
         return # ignore    
     if sender == UNKNOWN:
         if silent:
+            logging.debug(u"UNKNOWN ignoring %s@%s from %s" %
+                      (service_address, server_name, message['from']))
             return
         raise SMTPError(550, "Unknown sender")
 
-    # TODO verify the rollid exists and belongs to the sender
+    cid = sender[0]
+    lang = c.campaign_language(cid)
 
-    # TODO perform the roll
+    # verify the rollid exists and belongs to the sender
+    roll_obj = find_roll(rollid)
 
-    # TODO report back to the sender and GM (if they are different)
+    if roll_obj is None:
+        # croak no roll
+        msg = view.respond(locals(), "%s/roll/no_roll.msg" % (lang,),
+                            From="%s@%s" % (service_address, server_name),
+                            To=message['from'],
+                            Subject=view.render(locals(),
+                                                    "%s/no_roll.subj" % (lang,)))        
+        send_or_queue(msg, cid)
+        return
+    if roll_obj is True:
+        # croak already rolled, return result
+        ( roll, check, roll_str ) = execute_roll(rollid)
+        msg = view.respond(locals(), "%s/roll/already_rolled.msg" % (lang,),
+                            From="%s@%s" % (service_address, server_name),
+                            To=message['from'],
+                            Subject=view.render(locals(),
+                                                    "%s/already_rolled.subj" % (lang,)))
+        send_or_queue(msg, cid)
+        return
+
+    ( roll_cid, roll_character, roll_str ) = roll_obj
+
+    if str(roll_cid) != str(cid):
+        # croak spurious ID
+        msg = view.respond(locals(), "%s/roll/no_roll.msg" % (lang,),
+                            From="%s@%s" % (service_address, server_name),
+                            To=message['from'],
+                            Subject=view.render(locals(),
+                                                    "%s/no_roll.subj" % (lang,)))
+        send_or_queue(msg, cid)
+        return
+    
+    if not sender[1] and str(character) != sender[2]:
+        # croak not the right person for this roll
+        msg = view.respond(locals(), "%s/roll/wrong_person.msg" % (lang,),
+                            From="%s@%s" % (service_address, server_name),
+                            To=message['from'],
+                            Subject=view.render(locals(),
+                                                    "%s/wrong_person.subj" % (lang,)))
+        send_or_queue(msg, cid)
+        return
+
+    # perform the roll
+    ( roll, check ) = execute_roll(rollid)
+
+    # report back to the sender and GM (if they are different)
+    (gm_address, gm_full, attribution) = c.campaign_gm(cid)
+
+    # GM email
+    msg = view.respond(locals(), "%s/roll/rolled.msg" % (lang,),
+                           From="%s@%s" % (service_address, server_name,),
+                           To=gm_full,
+                           Subject=view.render(locals(),
+                                                   "%s/roll/rolled.subj"  % (lang,)))
+    if not sender[1]:
+        msg['cc'] = '%s@%s' % (roll_character, server_name,)
+        
+    send_or_queue(msg, cid)
+
+    if not sender[1]:
+        character = c.get_character(cid, roll_character)
+        attribution = c.get_attribution(character['controller'])
+        
+        msg = view.respond(locals(), "%s/rolled.msg" % (lang,),
+                               From="%s@%s" % (service_address, server_name,),
+                               To= formataddr( (attribution, character['controller']) ),
+                               Subject=view.render(locals(),
+                                                       "%s/rolled.subj"  % (lang,)))
+        msg['cc'] = 'gm@%s' % (server_name,)
+        send_or_queue(msg, cid)
+        
+    return
+        
+    
 
 @route("pm-roll@(host)")
 @stateless
 def ROLL(message, host=None):
+    service_address = 'pm-roll'
     sender = c.place_sender(message)
     if sender == INTERNAL:
         return # ignore    
@@ -31,51 +123,78 @@ def ROLL(message, host=None):
         if silent:
             return
         raise SMTPError(550, "Unknown sender")
-    if not sender[1]:
-        if silent:
-            return
-        raise SMTPError(550, "Only GMs can ask for rolls")
-
-    # TODO find the recipient that is a PC (if none, internal roll
-    # from the GM)
-
-    # TODO find the roll commands
-
-    # TODO for each roll command, register a roll-id and send a
-    # pm-dice-(rollid) email
     
+    if not sender[1]:
+        # croak only GMs can ask for rolls
+        msg = view.respond(locals(), "%s/roll/not_gm.msg" % (lang,),
+                               From="%s@%s" % (service_address, server_name,),
+                               To=message['from'],
+                               Subject=view.render(locals(),
+                                                    "%s/roll/not_gm.subj"  % (lang,)))
+        send_or_queue(msg, cid)
+        return
+    
+    (gm_address, gm_full, attribution) = c.campaign_gm(cid)
 
+    # find the recipient that is a PC (if none, internal roll from the GM)
+    recipients = c.get_recipients(message)
+    character = None
+    for recipient in recipients:
+        if recipient == 'gm':
+            continue
+        this_character = c.get_character(cid, recipient)
+        if not this_character['is_npc']:
+            if character is not None:
+                # croak only one character per roll
+                msg = view.respond(locals(), "%s/roll/too_many_characters.msg" % (lang,),
+                            From="%s@%s" % (service_address, server_name,),
+                            To=gm_full,
+                            Subject=view.render(locals(),
+                                                   "%s/roll/too_many_characters.subj"  % (lang,)))
+                send_or_queue(msg, cid)
+                return
+            character = recipient
 
-# dice entry point
-## @route("roll-(hashid)@(host)", hashid="[0-9]+")
-## def START(message, hashid=None, host=None):
-##     hashid = int(hashid)
-##     roll = find_roll(hashid)
-##     if roll is None:
-##         logging.info("Unknown roll %d" % (hashid))
-##         return
+    # find the roll commands
+    rolls = []
+    full_content = message.body()
+    text = safe_unicode(full_content)
+    lines = text.split('\n')
+    for line in lines:
+        if line.startswith('ROLL:'):
+            rolls.append(line[len('ROLL:'):])
+    if len(rolls) == 0:
+        # croak no rolls
+        msg = view.respond(locals(), "%s/roll/no_rolls.msg" % (lang,),
+                            From="%s@%s" % (service_address, server_name,),
+                            To=gm_full,
+                            Subject=view.render(locals(),
+                                                   "%s/roll/no_rolls.subj"  % (lang,)))
+        send_or_queue(msg, cid)
+        return
 
-##     outcome = message.body()
-##     set_roll_outcome(roll, outcome)
-##     logging.debug(u"Roll %d outcome for %s:\n%s" % (hashid, unicode(roll.target), outcome))
+    # for each roll command, register a roll-id and send a pm-dice-(rollid) email
+    if character is not None:
+        character = c.get_character(cid, character)
+        attribution = c.get_attribution(character['controller'])
+     
+    for roll in rolls:
+        try:
+            hashid = add_roll( cid, 'gm' if character is None else character, roll )
+            service_address = 'pm-dice-' + rollid
 
-##     # notify GM
-##     if not roll.campaign.gm.is_bouncing:
-##         character = find_character(roll.target, roll.campaign)
-##         mail = 'gm@%s' % (host,)
-        
-##         if character is None:
-##             mail = 'poisson-%d@%s' % (roll.target.user.id, server_name,)
-##             character = safe_unicode(roll.target)
-##         else:
-##             mail = character.mail_address
-##             character = safe_unicode(character)
-            
-##         outcome = safe_unicode(outcome)
-##         dice = view.respond(locals(), "dice.msg",
-##                                From=mail,
-##                                To=roll.campaign.gm.mail_address,
-##                                Subject=message['subject']
-##                                )
-##         relay.deliver(dice)
-##     return START
+            msg = view.respond(locals(), "%s/roll/to_roll.msg" % (lang,),
+                            From="%s@%s" % (service_address, server_name,),
+                            To=gm_full if character is None else character['controller'],
+                            Subject=view.render(locals(),
+                                                   "%s/roll/to_roll.subj"  % (lang,)))
+        except RollStrParseException as e:
+            msg = view.respond(locals(), "%s/roll/syntax_error.msg" % (lang,),
+                            From="%s@%s" % (service_address, server_name,),
+                            To=gm_full,
+                            Subject=view.render(locals(),
+                                                   "%s/roll/syntax_error.subj"  % (lang,)))
+        send_or_queue(msg, cid)
+    return
+
+    
--- app/model/dice.py
+++ app/model/dice.py
@@ -1,14 +1,147 @@
+from config.settings import server_name
+from email.utils import parseaddr, formataddr, getaddresses
+import table as t
+import logging
+import re
+import hashlib
+import random
+
+# data model:
+
+# roll-%rollid -> object ( campaign: %cid, character: %localpart, roll: str, rolled: value-str )
+# campaign-rolls-%cid -> list of %rollid
+
+# not-yet rolled rolls have no rolled field
+# the campaign-rolls-%cid are used for deleting all rolls at the end of a campaign
+
+class RollStrParseException(Exception):
+    pass
+
+ROLL_PATTERN = re.compile('^\s*(\d*D?\d+)(\s*[+-]\s*(\d*D?\d+))*\s*[\<\>]\s*\d+\s*$')
+
+
+def _parse(roll_str):
+    roll_str = roll_str.upper()
+    if ROLL_PATTERN.match(roll_str) is None:
+        raise RollStrParseException("Cannot parse '" + roll_str + "'")
+
+    is_less_than = '<' in roll_str
+    terms = re.split('\s*[\<\>]\s*', roll_str)
+    limit = int(terms[1])
+    
+    tokens0 = re.split('\s+', terms[0])
+    tokens1 = []
+    for tok in tokens0:
+        if '+' in tok:
+            plus_tok = tok.split('+')
+            first = True
+            for t in plus_tok:
+                if first:
+                    first = False
+                else:
+                    tokens1.append('+')
+                tokens1.append(t)
+        else:
+            tokens1.append(tok)
+    tokens = []
+    for tok in tokens1:
+        if '-' in tok:
+            minus_tok = tok.split('-')
+            first = True
+            for t in minus_tok:
+                if first:
+                    first = False
+                else:
+                    tokens.append('-')
+                tokens.append(t)
+        else:
+            tokens.append(tok)
+
+    parsed_tokens = []
+    is_plused = True
+
+    for tok in tokens:
+        if tok == '+':
+            is_plused = True
+        elif tok == '-':
+            is_plused = False
+        else:
+            parts = tok.split('D')
+            if len(parts) == 1:
+                parsed_tokens.append( ( is_plused, int(tok), 1) )  # 10 = 10D1
+            else:
+                parsed_tokens.append( ( is_plused, int(parts[0]), int(parts[1]) ) ) # 3D6
+
+    return (parsed_tokens, is_less_than, limit)
+
+def add_roll(campaign, character, roll_str):
+   """Adds a roll, might throw a RollStrParseException if the roll_str does not validate."""
+
+    _parse(roll_str) # validate
+
+    while True:
+        hashid = hashlib.sha256("%s-%s-%d" %
+                                   (cid, character, random.randint(0,9001))).hexdigest()[0:10]
+        roll_key = 'roll-%s' % (hashid,)
+        if not t.has_key(roll_key):
+            break
+
+    t.create_object(roll_key, { 'campaign' : campaign, 'character' : character, 'roll': roll_str })
+    t.add_to_list('campaign-' + campaign, hashid)
+    return hashid
+
+
+def _find_roll(hashid):
+    key = "roll-" + hashid
+    if not t.has_key(key):
+        return None
+    return t.get_object(key)
+    
 
 def find_roll(hashid):
-#    try:
-#        return Roll.objects.get(hashid=hashid)
-#    except Roll.DoesNotExist:
-        return None
+   """Find a roll and returns the campaign, character, roll_str or None if not found or True if already rolled"""
+    obj = _find_roll(hashid)
+    if obj is None:
+        return obj
+    if 'rolled' in obj:
+        return True
+    return ( obj['campaign'], obj['character'], obj['roll_str'] )
 
-def set_roll_outcome(roll, text):
-#    outcome = RollOutcome(roll=roll, outcome_text=text)
-#    outcome.save()
-#    roll.outcome = outcome
-#    roll.save()
-     return None
+
+def execute_roll(hashid):
+    """
+    Executes a roll, records it in the DB and returns its value, whether the check succeeded and its string form.
+    If already rolled, returns the existing value (as a pair value, whether it succeeded or not and its string form).
+    If the roll does not exist, returns None. 
+    """
+    obj = _find_roll(hashid)
+    if obj is None:
+        return obj
+    if 'rolled' in obj:
+        value_str, check = obj['rolled'].split(" ")
+        return ( int(value_str), bool(check), obj['roll'] )
+
+    # parse and execute the roll_str
+    ( parsed_tokens, is_less_than, limit ) = _parse( obj['roll'] )
+
+    r = random.Random()
+    r.seed(int(hashid, 16)) # the hash is the result
+
+    roll = 0
+    for tok in parsed_tokens:
+        value = 0
+        if tok[2] == 1:
+            value = tok[1]
+        else:
+            for v in range(tok[1]):
+                value += r.randint(1, tok[2])
+        if not tok[0]:
+            value = -value
+        roll += value
+
+    check =  (is_less_than and (roll < limit)) or (not is_less_than and (roll > limit))
+
+    t.set_field('roll-' + hashid, 'rolled', str(roll) + " " + str(check) )
+
+    return ( roll, check, obj['roll'] )
 
