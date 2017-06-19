8b86b402f02b6e5a49f5c625a346065ace3f84c8
Pablo Duboue <pablo.duboue@gmail.com>
2017-06-19 09:19:02 -0400

    Debugging

--- app/model/end_campaign.py
+++ app/model/end_campaign.py
@@ -9,7 +9,8 @@ import hashlib
 import random
 import codecs
 from subprocess import call
-from campaign import all_characters, campaign_gm, get_character, campaign_language, campaign_name, get_recipients
+from campaign import all_characters, campaign_gm, get_character, campaign_language, campaign_name, get_recipients, get_attribution, place_sender
+from emails import sanitize
 
 def end_campaign(cid, purge=True):
     """Ends a campaign, returns the list of email addresses to email,
@@ -21,8 +22,8 @@ def end_campaign(cid, purge=True):
     full_queue = Router.FULL_QUEUE
     messages = list()
 
-    name = campaign_name(cid)
-    lang = campaign_lang(cid)
+    campaign_name_ = campaign_name(cid)
+    lang = campaign_language(cid)
     emails_in_campaign = set()
     attribution_for_email = dict()
     ( gm_email, full_gm, gm_attribution ) = campaign_gm(cid)
@@ -38,8 +39,10 @@ def end_campaign(cid, purge=True):
 
     for key in full_queue.keys():
         msg = full_queue.get(key)
+        if msg is None:
+            continue
 
-        name, sender = parseadd(msg['from'])
+        name, sender = parseaddr(msg['from'])
         if sender in emails_in_campaign:
             msg_epoch = mktime_tz(parsedate_tz(msg['Date']))
             messages.append( { 'epoch' : msg_epoch, 'msg' : msg, 'key' : key } )
@@ -54,12 +57,12 @@ def end_campaign(cid, purge=True):
         target_zip = "%s/%s.zip" % ( campaigns_report_folder, nonce ) 
         if not os.path.exists(target_zip):
             break
-    tmp_folder = "/tmp/" % (nonce,)
+    tmp_folder = "/tmp/" + nonce
     os.mkdir(tmp_folder)
 
     # First pass, compute statistics
-    gm_email_as_npcs = 0
     gm_emails = 0
+    gm_emails_as_npcs = 0
     pc_emails = 0
     dice_rolls = 0
 
@@ -68,13 +71,16 @@ def end_campaign(cid, purge=True):
 
         name, sender = parseaddr(msg['From'])
         recepients = get_recipients(msg)
-        is_dice = False;
+        is_dice = False
+        rcpts = list()
         if 'pm-dice' in sender or 'pm-roll' in sender:
             is_dice = True
         else:
             for rcpt in recepients:
                 if 'pm-dice' in rcpt or 'pm-roll' in rcpt:
                     is_dice = True
+                elif not rcpt.startswith('as-'):
+                    rcpts.append(rcpt)
         if is_dice:
             dice_rolls += 1
             t['flags'] = { 'dice' : True }
@@ -84,14 +90,17 @@ def end_campaign(cid, purge=True):
                 t['flags'] = { 'gm' : True }
                 for rcpt in recepients:
                     if rcpt.startswith('as-'):
-                        gm_email_as_npcs += 1
+                        gm_emails_as_npcs += 1
                         t['flags']['as'] = rcpt[3:]
             else:
                 pc_emails += 1
-                placed = c.place_sender(msg)
-                if len(placed) == 2:
+                placed = place_sender(msg)
+                if len(placed) == 3:
                     short_form = placed[2]
                     t['flags'] = { 'pc' : short_form }
+                else:
+                    t['flags'] = 'UNKNOWN'
+        t['recipients'] = rcpts
         
     #### front matter
     tex = codecs.open(tmp_folder + "/campaign.tex", 'w', 'utf-8')
@@ -102,7 +111,7 @@ def end_campaign(cid, purge=True):
         tex_lang = "spanish"
 
     tex.write(u"""
-\documentclass[
+\\documentclass[
 oneside,
 openright,
 titlepage,
@@ -117,39 +126,39 @@ fontsize=11pt,
 american
 ]
 {scrreprt}
-\PassOptionsToPackage{utf8}{inputenc}
-\usepackage{inputenc}
-\usepackage[%s]{babel}
-\usepackage[pdfspacing]{classicthesis}
+\\PassOptionsToPackage{utf8}{inputenc}
+\\usepackage{inputenc}
+\\usepackage[%s]{babel}
+\\usepackage[pdfspacing]{classicthesis}
 
-\parskip 1.5ex
+\\parskip 1.5ex
 
-\begin{document}
-\selectlanguage{%s}
+\\begin{document}
+\\selectlanguage{%s}
 
 """ % (tex_lang,tex_lang))
     if lang == "en":
-        tex.write(u"""\title{%s}
-\subtitle{Poisson Magique Campaign Report}
-""" ^% (name,)) 
+        tex.write(u"""\\title{%s}
+\\subtitle{Poisson Magique Campaign Report}
+""" % (campaign_name_,)) 
     elif lang == "es":
-        tex.write(u"""\title{%s}
-\subtitle{Reporte de Juego en Poisson Magique}
-""" % (name,))
+        tex.write(u"""\\title{%s}
+\\subtitle{Reporte de Juego en Poisson Magique}
+""" % (campaign_name_,))
         
-    tex.write(u"""\maketitle
+    tex.write(u"""\\maketitle
 """)
 
     # GM, PC players, NPC players, attributions, licence
     
-    md.write(u"Poisson Magique Campaign: %s\n" % (name,))
+    md.write(u"Poisson Magique Campaign: %s\n" % (campaign_name_,))
     md.write(u"===========================================\n\n\n")
     md.write(u"Attributions\n")
     md.write(u"------------\n\n")
-    md.write(u"GM: %s\n" % gm_attribution)
+    md.write(u"GM: %s\n\n" % gm_attribution)
     for character in campaign_characters:
-        md.write(u'%s %s (%s): %s\n' % ("NPC" if int(character('is_npc')) else "PC",
-            character['fullname'], character['address'],
+        md.write(u'%s %s (%s): %s\n\n' % ("NPC" if int(character['is_npc']) else "PC",
+            character['name'], character['address'],
             character['alt_attribution'] if 'alt_attribution' in character else attribution_for_email[character['controller']]))
 
     md.write(u"\nAll content under license CC-BY-SA\n\n")
@@ -158,9 +167,13 @@ american
     md.write(u"Numbers\n")
     md.write(u"-------\n\n")
     md.write(u"""Number of emails received: %d
+
 GM emails: %d (as NPCs %d)
+
 PC emails: %d
+
 Dice roll emails: %d
+
 """ % (len(messages), gm_emails, gm_emails_as_npcs, pc_emails, dice_rolls))
 
     # TODO start date, end date
@@ -168,22 +181,29 @@ Dice roll emails: %d
     for t in messages:
         msg = t['msg']
 
-
         # header (includes who send it, potentially as-XYZ and to/cc)
+        rcpts = ",".join(t['recipients'])
         if 'dice' in t['flags']:
-            md.write("Dice Roll\n")
+            md.write("Dice Roll -> %s\n" % (rcpts,))
             md.write("---------\n")
         elif 'gm' in t['flags']:
             if 'as' in t['flags']:
-                md.write(t['flags']['as'] + u" (GM)\n")
+                md.write(u"%s (GM) -> %s\n" % (t['flags']['as'], rcpts))
                 md.write('------------------------\n')
+            else:
+                md.write(u"GM -> %s\n" % (rcpts,))
+                md.write('------------------------\n')                
         else:
-            md.write(t['flags']['pc'] + u" (PC)\n")
+            md.write(u"%s (PC) -> %s\n" % (t['flags']['pc'] , rcpts))
             md.write('------------------------\n')
 
+        md.write(t['key'] + "\n\n")
+        md.write(u"Subject: %s\n\n" % (msg['subject'],))
+        
         # content
         full_content = sanitize(msg.body())
         md.write(full_content)
+        md.write("\n")
 
         #TODO typeset email
 
@@ -198,7 +218,7 @@ Dice roll emails: %d
     #call("cd %s; pdflatex campaign.tex; pdflatex campaign.tex; pdflatex campaign.tex" % (tmp_folder,), shell=True)
 
     # zip source texts + PDF to target_zip
-    call("cd /tmp; zip -r %s %s" % (os.path.realpath(target_zip), nonce), shell=True)
+    call("cd /tmp; /usr/bin/zip -r %s %s" % (os.path.realpath(target_zip), nonce), shell=True)
 
     if purge:
         # delete messages from full queue
